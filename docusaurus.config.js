872778cdb3be0edad86f71e46b626ed252065247
Han Joo <han.joo@icloud.com>
2022-10-11 12:28:55 +0900

    title and description changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Docusaurus Test Website',
+  tagline: 'TESTING',
   url: 'https://github.com',
   baseUrl: '/TEST-docusaurus-website/',
   onBrokenLinks: 'throw',
