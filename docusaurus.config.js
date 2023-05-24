8a5e0d78dcd7fc00e6e7c522c9d5131f7fc92c22
Han Joo <han.joo@icloud.com>
2023-05-24 13:49:45 +0900

    Algolia DocSearch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'han joo.log',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -77,6 +77,35 @@ const config = {
             position: 'right',
           },
         ],
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'AQD12LMXP6',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'e910a1d3b15aeab9132f284baeff8fbd',
+  
+        indexName: 'Search',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
       },
       footer: {
         style: 'dark',
